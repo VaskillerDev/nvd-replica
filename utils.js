730cb163b071db0b93057abc7d6d1d7b577b971e
VaskillerDev <Vaskoooo9241@gmail.com>
2021-04-29 02:35:39 +0300

    init

---
+++ utils.js
@@ -0,0 +1,36 @@
+﻿'use strict'
+const qsParser = require('querystring');
+
+function trimParams(url) {
+    if (!url) return null;
+    try {
+        return url.split('?')[0];
+    } catch {
+        return null;
+    }
+}
+
+function trimPath(url) {
+    if (!url) return null;
+    try {
+        return url.split('?')[1];
+    } catch {
+        return null;
+    }
+}
+
+function eqStr(str0, str1) {
+    return str0.toLowerCase() === str1.toLowerCase()
+}
+
+function extractParams(url) {
+    url = trimPath(url);
+    return qsParser.parse(url);
+}
+
+module.exports = {
+    trimParams,
+    trimPath,
+    eqStr,
+    extractParams
+}
