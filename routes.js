f7854d38681e290ff782e679fe3f9e0c7b5a42c6
VaskillerDev <Vaskoooo9241@gmail.com>
2021-04-29 02:35:08 +0300

    init

---
+++ routes.js
@@ -0,0 +1,31 @@
+﻿'use strict'
+const {trimParams, eqStr, extractParams} = require("./utils");
+
+const routes = [
+    {path: '/getComponent', method: 'get', resolve: getComponent}
+]
+
+function getComponent(req, res) {
+    let {url} = req;
+    const {f,h,b} = extractParams(url);
+    
+    res.writeHead(200);
+    res.end('ok.');
+}
+
+function resolve(req, res) {
+    let {method, url} = req;
+    url = trimParams(url);
+    const maybeRoute = routes.find(route => eqStr(route.path, url) && eqStr(route.method, method));
+    
+    if (!maybeRoute) {
+        console.error(`route ${url} on method ${method} not found`);
+        return; // end
+    }
+    maybeRoute.resolve(req,res);
+}
+
+module.exports = {
+    routes: routes,
+    resolve: resolve
+}
