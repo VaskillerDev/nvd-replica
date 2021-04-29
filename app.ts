eb2a4ec72b2f4ed9bdaf41f7607cad0f788f3f22
VaskillerDev <Vaskoooo9241@gmail.com>
2021-04-29 03:30:50 +0300

    upd typing

--- app.ts
+++ app.ts
@@ -1,33 +1,38 @@
 ﻿const scheduler = require('node-schedule')
 const fs = require('fs')
-const https = require('https')
-const http = require('http')
+import https, { ServerOptions } from 'https'
+import http, { IncomingMessage, ServerResponse } from 'http'
 
 const { syncWithNVD } = require('./syncWithNVD')
 const routes = require('./routes')
 
-const time: any = []
+type SchedulerContainer = {
+    [key: string]: string
+}
+type MaybeBuffer = Buffer | string
+
+const time: SchedulerContainer = Object.create(null) as SchedulerContainer
 time['00:00:30'] = '30 * * * * *'
 time['04:00:30'] = '30 * 4 * * *'
 
 scheduler.scheduleJob(time['04:00:30'], syncWithNVD) // download cve archives in this time
 
 const port = 3000
-const key =
+const key: MaybeBuffer =
     fs.existsSync(process.env.PATH_TO_KEY) &&
     fs.readFileSync(process.env.PATH_TO_KEY)
-const cert =
+const cert: MaybeBuffer =
     fs.existsSync(process.env.PATH_TO_CERT) &&
     fs.readFileSync(process.env.PATH_TO_CERT)
-const options = { cert, key }
+const options: ServerOptions = { cert, key }
 const useHttps = key && cert
+
 function onStartCallback() {
     console.log('Port: ' + port, '\nHttps: ' + useHttps)
 }
-const server = useHttps ? https.createServer(options) : http.createServer()
 
-server.on('request', function (req: any, res: any) {
+const server = useHttps ? https.createServer(options) : http.createServer()
+server.on('request', function (req: IncomingMessage, res: ServerResponse) {
     routes.resolve(req, res)
 })
-
 server.listen(port, onStartCallback)
