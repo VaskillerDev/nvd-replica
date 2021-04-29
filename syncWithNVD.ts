d5d32b735723fddd5b2265180fef4668d4185031
VaskillerDev <Vaskoooo9241@gmail.com>
2021-04-29 04:59:53 +0300

    js -> ts, to es module

--- syncWithNVD.ts
+++ syncWithNVD.ts
@@ -1,29 +1,30 @@
-﻿'use strict'
-
-const path = require('path')
-const fs = require('fs')
-const https = require('https')
-const zlib = require('zlib')
+﻿import path from 'path'
+import fs from "fs";
+import https from "https";
+import zlib from "zlib";
 
 const startYear = 2002
-const endYear = 2005 //new Date().getFullYear();
+const endYear = new Date().getFullYear();
 const recent = 'recent'
 const modified = 'modified'
+const __dirname = path.resolve();
 const dirForData = path.resolve(__dirname, './cve')
+
+type Year = Date | number | string;
 
 function tryCreateDirForData() {
     fs.existsSync(dirForData) || fs.mkdirSync(dirForData)
 }
 
-function makeLinkToCveDownload(year) {
+function makeLinkToCveDownload(year : Year) {
     return `https://nvd.nist.gov/feeds/json/cve/1.1/nvdcve-1.1-${year}.json.gz`
 }
 
-function makePathToJsonGz(year) {
+function makePathToJsonGz(year : Year) {
     return path.resolve(dirForData, `nvdcve-1.1-${year}.json.gz`)
 }
 
-async function downloadJsonGz(link) {
+async function downloadJsonGz(link : string) {
     return new Promise(async resolve => {
         const downloadedFileName = path.basename(link)
         const pathToJsonGz = path.resolve(dirForData, downloadedFileName)
@@ -33,17 +34,17 @@ async function downloadJsonGz(link) {
     })
 }
 
-async function downloadByYear(year) {
+async function downloadByYear(year : Year) {
     const link = makeLinkToCveDownload(year)
     await downloadJsonGz(link)
     await console.log('download by ' + link)
 }
 
-function trimExt(fileName) {
+function trimExt(fileName : string) {
     return fileName.split('.').slice(0, -1).join('.')
 }
 
-async function unzipJsonGz(year) {
+async function unzipJsonGz(year : Year) {
     new Promise(resolve => {
         const pathToFile = makePathToJsonGz(year)
         const jsonPathToFile = trimExt(pathToFile)
@@ -52,7 +53,10 @@ async function unzipJsonGz(year) {
         const json = fs.createWriteStream(jsonPathToFile)
         const gunzip = zlib.createGunzip()
 
-        json.on('finish', () => fs.unlinkSync(pathToFile) && resolve(true))
+        json.on('finish', () => {
+            fs.unlinkSync(pathToFile);
+            resolve(true)
+        })
         jsonGz.pipe(gunzip).pipe(json)
     })
 }
@@ -85,6 +89,4 @@ async function syncWithNVD() {
     await unzipAllCve()
 }
 
-module.exports = {
-    syncWithNVD: syncWithNVD,
-}
+export default syncWithNVD;
