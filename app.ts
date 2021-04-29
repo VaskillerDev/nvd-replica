14274f30955416d315534ba073f29c7bc1b6d470
VaskillerDev <Vaskoooo9241@gmail.com>
2021-04-29 03:09:15 +0300

    js -> ts, fmt

---
+++ app.ts
@@ -0,0 +1,33 @@
+﻿const scheduler = require('node-schedule')
+const fs = require('fs')
+const https = require('https')
+const http = require('http')
+
+const { syncWithNVD } = require('./syncWithNVD')
+const routes = require('./routes')
+
+const time: any = []
+time['00:00:30'] = '30 * * * * *'
+time['04:00:30'] = '30 * 4 * * *'
+
+scheduler.scheduleJob(time['04:00:30'], syncWithNVD) // download cve archives in this time
+
+const port = 3000
+const key =
+    fs.existsSync(process.env.PATH_TO_KEY) &&
+    fs.readFileSync(process.env.PATH_TO_KEY)
+const cert =
+    fs.existsSync(process.env.PATH_TO_CERT) &&
+    fs.readFileSync(process.env.PATH_TO_CERT)
+const options = { cert, key }
+const useHttps = key && cert
+function onStartCallback() {
+    console.log('Port: ' + port, '\nHttps: ' + useHttps)
+}
+const server = useHttps ? https.createServer(options) : http.createServer()
+
+server.on('request', function (req: any, res: any) {
+    routes.resolve(req, res)
+})
+
+server.listen(port, onStartCallback)
