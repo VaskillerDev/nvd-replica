bb4ee9868f8a0ad5586ed3cd990a97e09bbde3d8
VaskillerDev <Vaskoooo9241@gmail.com>
2021-05-09 19:53:43 +0300

    rewrite

--- src/searchInStorage.ts
+++ src/searchInStorage.ts
@@ -1,15 +1,28 @@
 ﻿import fs from 'fs'
 import path from 'path'
 import readline from 'readline'
+import { StorageType } from './types/StorageType'
 
+const startYear = 2002
+const endYear = new Date().getFullYear()
 const __dirname = path.resolve()
 const dirForData = path.resolve(__dirname, './cve')
-const pathToStorage = dirForData + path.sep + 'cveStorage.csv'
+const recent = 'recent'
+const modified = 'modified'
 
-async function searchInStorage(keyword: string): Promise<Array<string>> {
+type Year = Date | number | string
+
+function makePathToStorageCsv(year: Year) {
+    return path.resolve(dirForData, `nvdcve-1.1-${year}.storage.csv`)
+}
+
+async function collectDataFromStorageCsv(
+    pathToStorageCsv: string,
+    keyword: string
+): Promise<Array<string>> {
     return new Promise(resolve => {
         const regex = new RegExp(keyword.toLowerCase())
-        const rs = fs.createReadStream(pathToStorage)
+        const rs = fs.createReadStream(pathToStorageCsv as string)
         const rl = readline.createInterface({
             input: rs,
             crlfDelay: Infinity,
@@ -24,6 +37,42 @@ async function searchInStorage(keyword: string): Promise<Array<string>> {
         rl.on('close', function () {
             resolve(storage)
         })
+    })
+}
+
+async function collectDataFromStorageCsvByYear(
+    year: Year,
+    keyword: string
+): Promise<Array<string>> {
+    return new Promise(async resolve => {
+        const pathToStorageCsv = makePathToStorageCsv(year)
+        const dataFromStorageCsv = await collectDataFromStorageCsv(
+            pathToStorageCsv,
+            keyword
+        )
+        resolve(dataFromStorageCsv)
+    })
+}
+
+async function searchInStorage(
+    storageType: StorageType,
+    keyword: string
+): Promise<Array<string>> {
+    return new Promise(async resolve => {
+        let storage: Array<string> = []
+        for (let year = startYear; year <= endYear; year++) {
+            storage = storage.concat(
+                await collectDataFromStorageCsvByYear(year, keyword)
+            )
+        }
+        storage = storage.concat(
+            await collectDataFromStorageCsvByYear(recent, keyword)
+        )
+        storage = storage.concat(
+            await collectDataFromStorageCsvByYear(modified, keyword)
+        )
+        console.log(storage)
+        resolve(storage)
     })
 }
 
