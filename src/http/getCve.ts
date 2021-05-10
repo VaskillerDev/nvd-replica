13c7d36ad510a030db8b650aaa28a838ee920a64
VaskillerDev <Vaskoooo9241@gmail.com>
2021-05-10 17:11:48 +0300

    mv getCve(), types, +translateRequest(), +matchRoutes()

---
+++ src/http/getCve.ts
@@ -0,0 +1,29 @@
+﻿import {IncomingMessage, ServerResponse} from "http";
+import {extractParams} from "../utils";
+import searchInStorage from "../searchInStorage";
+import {StorageType} from "../types/StorageType";
+import {Stream} from "node:stream";
+import {ContentType} from "../types/HttpTypes";
+
+export default function getCve(req: IncomingMessage, res: ServerResponse) {
+    let { url } = req
+    const { soft } = extractParams(url as string) as { soft: string }
+
+    if (soft === '' || !soft) {
+        res.setHeader('Content-Type', ContentType.JSON)
+        res.writeHead(404)
+        const result = { result: [] }
+        res.end(JSON.stringify(result))
+        return
+    }
+
+    searchInStorage(StorageType.Csv, soft).then(searchRes => {
+        const rs = new Stream.Readable()
+        const result = { result: searchRes }
+        res.setHeader('Content-Type', ContentType.JSON)
+        res.writeHead(200)
+        rs.pipe(res)
+        rs.push(JSON.stringify(result))
+        rs.push(null)
+    })
+}
--- src/routes.ts
+++ src/routes.ts
@@ -1,50 +1,32 @@
 ﻿import { IncomingMessage, ServerResponse } from 'http'
-import { eqStr, extractParams, trimParams } from './utils'
-import searchInStorage from './searchInStorage'
-import { StorageType } from './types/StorageType'
-import { Stream } from 'node:stream'
+import { lowerCaseDeepEqual, trimParams} from './utils'
+import {Route, RouteResolve} from "./types/HttpTypes";
+import getCve from "./http/getCve";
+import {Method} from "./types/HttpTypes";
 
-type Method = 'post' | 'get' | 'put' | 'delete'
-type ResolveFuncSignature = (req: IncomingMessage, res: ServerResponse) => void
-type Route = { path: string; method: Method; resolve: ResolveFuncSignature }
-
-const routes: Array<Route> = [
-    { path: '/cve', method: 'get', resolve: getCve }, // ?soft=nginx
+const routes: Array<RouteResolve> = [
+    { path: '/cve', method: Method.GET, resolve: getCve }, // ?soft=nginx
 ]
 
-function getCve(req: IncomingMessage, res: ServerResponse) {
-    let { url } = req
-    const { soft } = extractParams(url as string) as { soft: string }
-
-    if (soft === '' || !soft) {
-        res.setHeader('Content-Type', 'application/json;charset=utf-8')
-        res.writeHead(404)
-        const result = { result: [] }
-        res.end(JSON.stringify(result))
-        return
-    }
-
-    searchInStorage(StorageType.Csv, soft).then(searchRes => {
-        const rs = new Stream.Readable()
-        const result = { result: searchRes }
-        res.setHeader('Content-Type', 'application/json;charset=utf-8')
-        res.writeHead(200)
-        rs.pipe(res)
-        rs.push(JSON.stringify(result))
-        rs.push(null)
-    })
-}
-
 export function resolve(req: IncomingMessage, res: ServerResponse) {
-    let { method, url } = req as { method: string; url: string }
-    url = trimParams(url) as string
-    const maybeRoute = routes.find(
-        route => eqStr(route.path, url) && eqStr(route.method, method)
-    )
-
+    let incomingRoute : Route = translateRequest(req);
+    incomingRoute.path = trimParams(incomingRoute.path) as string
+    
+    const maybeRoute = routes.find(recordedRoute => matchRoutes(incomingRoute, recordedRoute))
+    
     if (!maybeRoute) {
-        console.error(`route ${url} on method ${method} not found`)
+        console.error(`route ${incomingRoute.path} on ${incomingRoute.method} not found`)
         return // end
     }
+    
     maybeRoute.resolve(req, res)
+}
+
+function translateRequest<T extends Route>(req: IncomingMessage) : T {
+    let { method, url : path } = req as { method: Method; url: string }
+    return {method, path} as T
+}
+
+function matchRoutes(incomingRoute : Route, recordedRoute : Route) : boolean {
+    return lowerCaseDeepEqual(incomingRoute,recordedRoute)
 }
