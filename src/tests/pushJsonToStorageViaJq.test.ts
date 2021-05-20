cce321bc153bc8cd712fdeb712979aaa926d400b
VaskillerDev <Vaskoooo9241@gmail.com>
2021-05-20 18:45:47 +0300

    +test

---
+++ src/tests/pushJsonToStorageViaJq.test.ts
@@ -0,0 +1,9 @@
+﻿import pushJsonToStorage, { Parser } from '../pushJsonToStorage'
+import path from 'path'
+
+;(() => {
+    const __dirname = path.resolve()
+    const pathToFile = path.resolve(__dirname, './cve/nvdcve-1.1-2003.json')
+
+    pushJsonToStorage(pathToFile, Parser.Jq)
+})()
